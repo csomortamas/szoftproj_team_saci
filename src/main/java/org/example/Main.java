3e27d145db875498196420a3acaf93029cbe92fa
horvi7 <adam.horvath3232@edu.bme.hu>
2023-03-04 00:50:27 +0100

    Csak proba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package main.java.org.example;
 import org.tinylog.Logger;
 
-
-
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("proba commit");
         Logger.info("Proba");
         System.out.println("Hello world!");
         System.out.println("Szia helo ");

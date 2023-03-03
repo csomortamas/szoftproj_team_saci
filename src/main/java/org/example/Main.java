87ecbbc468e7567b1130933279a0ead20b6a7089
horvi7 <adam.horvath3232@edu.bme.hu>
2023-03-04 00:04:43 +0100

    Tinylog Dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package main.java.org.example;
+import org.tinylog.Logger;
+
+
 
 public class Main {
     public static void main(String[] args) {
 
+        Logger.info("Proba");
         System.out.println("Hello world!");
         System.out.println("Szia helo ");
     }

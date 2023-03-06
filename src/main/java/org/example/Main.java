e3fb8ba8b85fb3ce669e1eec80e9441b170cd035
mackmarton <mackmarton@edu.bme.hu>
2023-03-06 11:44:48 +0100

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import org.tinylog.Logger;
 public class Main {
     public static void main(String[] args) {
         System.out.println("proba commit");
-        System.out.println("Marci voltam");
         Logger.info("Proba");
         System.out.println("Hello world!");
         System.out.println("Szia helo ");

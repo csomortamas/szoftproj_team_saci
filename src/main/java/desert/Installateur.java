2ced9a325f60f4bca621834740beb61a8565a797
Mack Marton <mackmarton@edu.bme.hu>
2023-03-31 00:22:05 +0200

    fix Rohr

--- src/main/java/desert/Installateur.java
+++ src/main/java/desert/Installateur.java
@@ -2,6 +2,9 @@ package main.java.desert;
 import lombok.Getter;
 import lombok.Setter;
 import org.tinylog.Logger;
+
+import java.util.ArrayList;
+
 /**
  *
  */
@@ -10,32 +13,47 @@ public class Installateur extends Spieler {
      * Objekt von Pumpe in der Hand von der Installateur.
      */
     @Getter @Setter private Pumpe pumpeInHand;
+
+    private Kontroller kontroller;
+
     /**
      * Default Konstruktor
      */
-    public Installateur() {
+    public Installateur(Pumpe startPunkt, Kontroller k) {
+        aktuellePumpe = startPunkt;
+        kontroller = k;
+        Logger.info("Installateur erstellt!");
     }
     
     /**
      *
      */
     public void reparieren() {
-        // TODO implement here
+        if (aktuelleRohr!=null){
+            aktuelleRohr.setIstKaputt(false);
+            Logger.info("Rohr repariert!");
+        } else{
+            aktuellePumpe.setIstKaputt(false);
+            Logger.info("Pumpe repariert!");
+        }
     }
 
-    /**
-     *
-     */
-    public void rohrReparieren() {
-        // TODO implement here
-    }
 
     /**
      * @param pumpeWohin
      *
      */
     public void rohrMitFreiemEndeBinden(Pumpe pumpeWohin) {
-        // TODO implement here
+        if (aktuelleRohr!=null){
+            if (aktuelleRohr.getEndPumpen().size()==1){
+                aktuelleRohr.getEndPumpen().add(pumpeWohin);
+                Logger.info("Rohr repariert!");
+            } else {
+                Logger.error("Beide Enden des Rohres sind bereits mit einer Pumpe verbunden.");
+            }
+        } else {
+            Logger.error("Der Spieler steht derzeit nicht auf eine Rohr.");
+        }
     }
 
     /**
@@ -43,7 +61,47 @@ public class Installateur extends Spieler {
      *
      */
     public void pumpeEinmontieren(boolean middle) {
-        // TODO implement here
+        if(pumpeInHand!=null){
+            if (middle){
+                if (aktuelleRohr!=null){
+                    ArrayList<Rohr> rohren = (ArrayList<Rohr>) aktuelleRohr.rohrSplit();
+
+                    aktuelleRohr.getEndPumpen().get(0).removeRohr(aktuelleRohr);
+                    rohren.get(0).getEndPumpen().add(aktuelleRohr.getEndPumpen().get(0));
+                    aktuelleRohr.getEndPumpen().get(0).addRohr(rohren.get(0));
+
+                    aktuelleRohr.getEndPumpen().get(1).removeRohr(aktuelleRohr);
+                    rohren.get(1).getEndPumpen().add(aktuelleRohr.getEndPumpen().get(1));
+                    aktuelleRohr.getEndPumpen().get(1).addRohr(rohren.get(1));
+
+                    kontroller.addPumpe(pumpeInHand);
+                    pumpeInHand.addRohr(rohren.get(0));
+                    pumpeInHand.addRohr(rohren.get(1));
+                } else {
+                    Logger.error("Der Spieler steht derzeit nicht auf eine Rohr.");
+                }
+            } else {
+                if (aktuelleRohr!=null){
+                    ArrayList<Rohr> rohren = (ArrayList<Rohr>) aktuelleRohr.rohrSplit();
+
+                    aktuelleRohr.getEndPumpen().get(0).removeRohr(aktuelleRohr);
+                    rohren.get(0).getEndPumpen().add(aktuelleRohr.getEndPumpen().get(0));
+                    aktuelleRohr.getEndPumpen().get(0).addRohr(rohren.get(0));
+
+                    aktuelleRohr.getEndPumpen().get(1).removeRohr(aktuelleRohr);
+                    rohren.get(1).getEndPumpen().add(aktuelleRohr.getEndPumpen().get(1));
+                    aktuelleRohr.getEndPumpen().get(1).addRohr(rohren.get(1));
+
+                    kontroller.addPumpe(pumpeInHand);
+                    pumpeInHand.addRohr(rohren.get(0));
+                    pumpeInHand.addRohr(rohren.get(1));
+                } else {
+                    Logger.error("Der Spieler steht derzeit nicht auf eine Rohr.");
+                }
+            }
+        } else {
+            Logger.error("Dieser Spieler hat keine Pumpe in der Hand.");
+        }
     }
 
     /**
--- src/main/java/desert/Spieler.java
+++ src/main/java/desert/Spieler.java
@@ -7,28 +7,38 @@ import org.tinylog.Logger;
  */
 public abstract class Spieler {
     /**
-     * Aktuelle Position der Spieler.
+     *
      */
-    @Getter @Setter protected Netzelement akutellePosition;
+    @Getter @Setter protected Pumpe aktuellePumpe;
     /**
-     * Default Konstruktor
+     *
      */
-    public Spieler() {
-    }
+    @Getter @Setter protected Rohr aktuelleRohr;
+
+
 
     /**
      *
      */
     public void pumpeStarten() {
-        // TODO implement here
+        if (aktuellePumpe!=null){
+            aktuellePumpe.setIstAktiv(true);
+            Logger.info("Pumpe gestartet!");
+        } else {
+            Logger.error("Der Spieler steht derzeit nicht auf einer Pumpe.");
+        }
     }
 
     /**
      * @param rohr
-     *
      */
     public void eingangsRohrUmstellen(Rohr rohr) {
-        // TODO implement here
+        if (aktuellePumpe!=null){
+            aktuellePumpe.setEingangsRohr(rohr);
+            Logger.info("Eingangsrohr umgestellt!");
+        } else {
+            Logger.error("Der Spieler steht derzeit nicht auf einer Pumpe.");
+        }
     }
 
     /**
@@ -36,21 +46,44 @@ public abstract class Spieler {
      *
      */
     public void ausgangsRohrUmstellen(Rohr rohr) {
-        // TODO implement here
+        if (aktuellePumpe!=null){
+            aktuellePumpe.setEingangsRohr(rohr);
+            Logger.info("Eingangsrohr umgestellt!");
+        } else {
+            Logger.error("Der Spieler steht derzeit nicht auf einer Pumpe.");
+        }
     }
 
     /**
      *
      */
     public void pumpeStoppen() {
-        // TODO implement here
+        if (aktuellePumpe!=null){
+            aktuellePumpe.setIstAktiv(false);
+            Logger.info("Pumpe gestoppt!");
+        } else {
+            Logger.error("Der Spieler steht derzeit nicht auf einer Pumpe.");
+        }
     }
 
     /**
      * @param netzwerkElement
-     *
      */
     public void step(Netzelement netzwerkElement) {
-        // TODO implement here
+        if (aktuellePumpe!=null){
+            if (aktuellePumpe.getClass() != netzwerkElement.getClass()){
+                setAktuelleRohr((Rohr) netzwerkElement);
+                Logger.info("Der Spieler ist auf ein neues Feld gezogen!");
+            }else {
+                Logger.error("Von einen Rohr können wir nur auf eine Pumpe treten.");
+            }
+        } else {
+            if (aktuelleRohr.getClass() != netzwerkElement.getClass()){
+                setAktuellePumpe((Pumpe) netzwerkElement);
+                Logger.info("Der Spieler ist auf ein neues Feld gezogen!");
+            }else {
+                Logger.error("Von einen Pumpe können wir nur auf eine Rohr treten.");
+            }
+        }
     }
 }

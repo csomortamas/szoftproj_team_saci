09da48a27823fa95b2b46c93402adb9361545609
horvi7 <adam.horvath3232@edu.bme.hu>
2023-03-30 23:05:33 +0200

    Pumpe felig

--- src/main/java/desert/Pumpe.java
+++ src/main/java/desert/Pumpe.java
@@ -1,41 +1,55 @@
 package main.java.desert;
+
 import lombok.Getter;
 import lombok.Setter;
 import org.tinylog.Logger;
+
 import java.util.List;
 
 public class Pumpe extends Netzelement {
     /**
      *
      */
-    @Getter @Setter private List<Rohr> rohre;
+    @Getter
+    @Setter
+    private List<Rohr> rohre;
 
     /**
      *
      */
-    @Getter @Setter private Rohr eingangsRohr;
+    @Getter
+    @Setter
+    private Rohr eingangsRohr;
     /**
      *
      */
-    @Getter @Setter private Rohr ausgangsRohr;
+    @Getter
+    @Setter
+    private Rohr ausgangsRohr;
 
 
     /**
      *
      */
-    @Getter @Setter private int wasserTank;
+    @Getter
+    @Setter
+    private int wasserTank;
 
     /**
      *
      */
 
 
-    @Getter @Setter private int maxRohrAnzahl;
+    @Getter
+    @Setter
+    private int maxRohrAnzahl;
 
     /**
      * Default Konstruktor
      */
     public Pumpe() {
+        Pumpe p = new Pumpe();
+        Logger.info("Neue Pumpe erstellt.");
     }
 
     /**
@@ -43,6 +57,11 @@ public class Pumpe extends Netzelement {
      */
     public void tankFuellen() {
         // TODO implement here
+        if (this.ausgangsRohr.istKaputt || this.ausgangsRohr == null || this.eingangsRohr.getFlusswert() > this.ausgangsRohr.getFlusswert()) {
+            int plusWasser = this.eingangsRohr.getFlusswert() - this.ausgangsRohr.getFlusswert();
+            this.wasserTank += plusWasser;
+            Logger.info("Die WassermAnge in dieser Pumpe wurde mit " + plusWasser + "erhöht");
+        }
     }
 
     /**

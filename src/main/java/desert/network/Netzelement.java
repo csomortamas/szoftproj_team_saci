e22e33411cc1187e6a53b2e982db19b308f73221
Csomor Tamás <52610278+csomortamas@users.noreply.github.com>
2023-04-21 17:46:07 +0200

    Update Netzelement.java

--- src/main/java/desert/network/Netzelement.java
+++ src/main/java/desert/network/Netzelement.java
@@ -14,6 +14,10 @@ public abstract class Netzelement {
     /**
      *
      */
+    @Getter @Setter protected String name;
+    /**
+     *
+     */
     @Getter @Setter protected boolean istAktiv;
 
     /**

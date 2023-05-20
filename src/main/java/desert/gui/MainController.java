c0198c3de7389fcead2bd652ea3e8536881b034b
Mack Marton <mackmarton@edu.bme.hu>
2023-05-20 15:22:06 +0200

    Tweaks

--- src/main/java/desert/gui/MainController.java
+++ src/main/java/desert/gui/MainController.java
@@ -146,6 +146,7 @@ public class MainController {
 
         GuiMap.getGuiMap().refreshPlayerButtons();
         GuiMap.getGuiMap().refreshControlPanes();
+        GuiMap.getGuiMap().refreshRoehre();
     }
 
     public class LineClickAction implements EventHandler<MouseEvent> {
